00a8aff9ef1cf93911df26cadd6528c9442b6f7c
SK <microfixbg@gmail.com>
2025-06-14 18:46:43 +0300

    Protect User routes
    Protect User routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('../controllers/userController');
-  
-router.get('/', getAllUsers);
-router.get('/:id', getUserById);
-router.post('/', createUser);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser); 
+const { verifyToken } = require('../middlewares/auth');
+
+router.get('/', verifyToken, getAllUsers);
+router.get('/:id', verifyToken, getUserById);
+router.post('/', verifyToken, createUser);
+router.put('/:id', verifyToken, updateUser);
+router.delete('/:id', verifyToken, deleteUser); 
  
 module.exports = router;
