43438b0bbf0dfd64b1e852667979f328e72313fd
SK <microfixbg@gmail.com>
2025-06-15 14:06:24 +0300

    .

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createPayment, updatePayment, deletePayment } = require('../controllers/paymentController');
+const { createPayment, listPayments, updatePayment, deletePayment } = require('../controllers/paymentController');
 
 router.post('/', createPayment);
 router.get('/', listPayments);
